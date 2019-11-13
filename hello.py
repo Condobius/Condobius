c30575438cb0cac2a4f7628ae139aefab28165db
Keeley <370586@mcpsmd.org>
2019-11-13 09:12:37 -0500

    Added question about age and an age checker

--- hello.py
+++ hello.py
@@ -1,2 +1,7 @@
 name = input("What is your name?")
+age = int(input("How old are you?"))
 print("Hello" + name + "!")
+if age >= 13 and age <= 19:
+     print ("You are a teenager")
+elif age >= 13:
+    print ("You are a child")

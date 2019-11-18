365883681735da2160643f69e30d0f0ee16c138b
Keeley <370586@mcpsmd.org>
2019-11-18 09:09:12 -0500

    [I took different code and put it in here to make sure my bruh.py would function correctly]

--- hello.py
+++ hello.py
@@ -1,12 +1,8 @@
 name = input("What is your name?")
-<<<<<<< HEAD
+town = input("Where do you live?")
+print("Hello" + name + " from" + town + "!")
 age = int(input("How old are you?"))
-print("Hello" + name + "!")
 if age >= 13 and age <= 19:
      print ("You are a teenager")
 elif age >= 13:
     print ("You are a child")
-=======
-town = input("Where do you live?")
-print("Hello" + name + " from" + town + "!")
->>>>>>> location
